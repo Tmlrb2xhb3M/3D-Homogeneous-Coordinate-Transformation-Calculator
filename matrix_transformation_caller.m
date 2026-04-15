7aa3b8db9bef2ab0d3bdc32d022ad6a02c5623c1
Nikolaos Karteris <nikos.karteris28@gmail.com>
2026-04-15 12:01:34 +0300

    Created special caller

---
+++ matrix_transformation_caller.m
@@ -0,0 +1,15 @@
+function retval = matrix_transformation_caller (coordinate_matrix, vector, transformation)
+  switch(transformation)
+    case 'r'
+      retval = matrix_rotation_homogeneous_3d(coordinate_matrix, vector);
+
+    case 'sc'
+      retval = matrix_scaling_homogeneous_3d(coordinate_matrix, vector);
+
+    case 'sh'
+      retval = matrix_shearing_homogeneous_3d(coordinate_matrix, vector);
+
+    case 't'
+      retval = matrix_translation_homogeneous_3d(coordinate_matrix, vector);
+  endswitch
+endfunction
