02c7906de04d124ef95e86e474e26e2a79383b99
Nikolaos Karteris <nikos.karteris28@gmail.com>
2026-04-17 13:28:24 +0300

    The caller can calculate coordinates for multiple coordinates at the same time

--- matrix_transformation_caller.m
+++ matrix_transformation_caller.m
@@ -1,31 +1,34 @@
 function retval = matrix_transformation_caller (coordinate_matrix, vectors, transformations)
-  counter = 1;
-  temp = coordinate_matrix;
-  for i = 1:length(transformations)
-    switch(transformations(i))
-      case 's'
-        continue
+  retval = [;;;];
+  for i = 1:columns(coordinate_matrix)
+    counter = 1;
+    temp = coordinate_matrix(:,i);
+    for j = 1:length(transformations)
+      switch(transformations(j))
+        case 's'
+          continue
 
-      case 'r'
-        temp = matrix_rotation_homogeneous_3d(temp, vectors(counter,:));
-        counter = counter + 1;
+        case 'r'
+          temp = matrix_rotation_homogeneous_3d(temp, vectors(counter,:));
+          counter = counter + 1;
 
-      case 'c'
-        temp = matrix_scaling_homogeneous_3d(temp, vectors(counter,:));
-        counter = counter + 1;
+        case 'c'
+          temp = matrix_scaling_homogeneous_3d(temp, vectors(counter,:));
+          counter = counter + 1;
 
-      case 'h'
-        temp = matrix_shearing_homogeneous_3d(temp, vectors(counter,:));
-        counter = counter + 1;
+        case 'h'
+          temp = matrix_shearing_homogeneous_3d(temp, vectors(counter,:));
+          counter = counter + 1;
 
-      case 't'
-        temp = matrix_translation_homogeneous_3d(temp, vectors(counter,:));
-        counter = counter + 1;
+        case 't'
+          temp = matrix_translation_homogeneous_3d(temp, vectors(counter,:));
+          counter = counter + 1;
 
-      otherwise
-        error('Invalid operation, expected "r", "sc", "sh", "t"')
-        return
-    endswitch
+        otherwise
+          error('Invalid operation, expected "r", "sc", "sh", "t"')
+          return
+      endswitch
+    endfor
+    retval = [retval temp];
   endfor
-  retval = temp;
 endfunction
